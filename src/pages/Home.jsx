bae89d87f780a0f98ac2ec9caebcc7960c467f39
aditiaz <aditiaan96a@gmail.com>
2023-01-23 22:47:51 +0700

    done I think

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import * as Components from "../components";
 import { Row } from "react-bootstrap";
+import IndexOwner from "./IndexOwner";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const navigate = useNavigate();
+  const roles = localStorage.Roles;
+
+  useEffect(() => {
+    navigate("/");
+  }, [roles]);
+
   return (
     <div>
       <Components.Navbars />
-      <Row>
+      {roles === "Admin" ? (
+        <IndexOwner />
+      ) : (
+        <Row>
+          <Components.SideBar />
+          <Components.RoomsCard />
+        </Row>
+      )}
+      {/* <Components.Navbars /> */}
+      {/* <Row>
         <Components.SideBar />
         <Components.RoomsCard />
-      </Row>
+      </Row> */}
     </div>
   );
 };
